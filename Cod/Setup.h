c7d555f1d426531c9f67f1e8d099be4b62ee87a2
isaacmarlon <isaacmarlondasilvalourenco@gmail.com>
2018-04-10 23:34:35 -0300

    Setup.h - 0.1

--- Cod/Setup.h
+++ Cod/Setup.h
@@ -1,18 +1,21 @@
 #ifndef SETUP_H
 #define SETUP_H
 
-#include <robo_hardwareEva.h>
-/*
-# <Arduino.h>
-# <Servo.h>
+/**** Sobre:
+	 
+	A classe Setup foi feita para organizar e simplificar a utilização
+	mútua das classes. Supre a necessidade de setar algum valor ou até mesmo
+	alguma função indo até a classe respectiva.
 
-# "cor.h"
-# "pinagem.h"
-# "Ultrasonic.h"
+	Quando incluímos "Setup.h" em outra classe, é possível criar objetos de outras
+	classes sem a necessidade de inclusão.
 
-# "EEPROM2.h"
-*/
-#include "Calibracao.h"
+****/
+
+#include <robo_hardware.h>
+
+//#include "Calibracao.h"
+#include "Estrategia.h"
 #include "Sensores.h"
 #include "Sonares.h"
 #include "Movimentacao.h"
@@ -21,10 +24,29 @@
 
 class Setup{
 public: 
-	void leds(bool value);
-	void calibrar(bool value);
-};
 
-//static Setup set;
+	void leds(bool value) { // Define a pinagem para os LEDs.
+		if (value) 
+		leds.pinar(); 
+	} 
+	void calibrar(bool value) { // Entra no modo de Calibração.
+		if (value)
+		calibrar.iniciar(); 
+	} 
+	
+	void motoresVelPadrao(int VEL_SET) { // Seta o VELPADRAO em "Motores.h".
+		motores.setVelPadrao(VEL_SET); 
+	} 	
+	void distanciaObstaculo(int DISTANCIA_SET) { // Seta DISTANCIA_OBSTACULO em ...
+		obstaculo.setDistancia(DISTANCIA_SET);   // ... "Obstaculo.h".
+	}
+
+private:
+
+	Leds leds;
+	//Calibrar calibrar;
+	Motores motores;
+	Obstaculo obstaculo;
+};
 
 #endif
