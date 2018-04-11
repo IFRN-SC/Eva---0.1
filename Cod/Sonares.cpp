bba36feabc19f292c5f77e351d93305f4410cd5f
isaacmarlon <isaacmarlondasilvalourenco@gmail.com>
2018-04-10 23:38:08 -0300

    Deletado "Sonares.cpp"

--- Cod/Sonares.cpp
+++
@@ -1,9 +0,0 @@
-#include "Sonares.h"
-
-bool Sonares::viuObstaculo(int DISTANCIA_OBSTACULO){
-	if (robo.lerSensorFrontal() <= DISTANCIA_OBSTACULO){
-		return true;
-	} else {
-		return false;
-	}
-}
