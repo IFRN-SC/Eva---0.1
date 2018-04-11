8b6b891a2c2ac7c99f5b2a44d68dc7a9f5e6ad45
isaacmarlon <isaacmarlondasilvalourenco@gmail.com>
2018-04-10 22:36:50 -0300

    Movimentacao.h - 0.1

--- Cod/Movimentacao.h
+++ Cod/Movimentacao.h
@@ -3,17 +3,44 @@
 
 #include "Setup.h"
 
-#define VELPADRAO 40 // Velocidade padrão.
-//#define DISTANCIA 0 
-
-class Movimentacao{
+class Movimentacao {
+private: const int VELPADRAO;
 public:
-	void emFrente(){robo.motores(VEL,VEL);}
-	void virarEsquerda(){robo.motores(VEL*(-1), VEL);}
-	void virarDireita(){robo.motores(VEL,VEL*(-1));}
-	void alinhar(int sensor);
-	void parar(int tempo);
-	void recuar(int tempo);
+	/**** Funções de movimentação básica ****/
+	
+	void emFrente() { robo.acionarMotores(VELPADRAO,VELPADRAO); }
+	void virarEsquerda() { robo.acionarMotores(VELPADRAO *(-1), VELPADRAO); }
+	void virarDireita() { robo.acionarMotores(VELPADRAO,VELPADRAO *(-1)); }
+
+	void parar(int tempo) {
+		robo.acionarMotores(0,0); 
+		delay(tempo);
+	}
+	void recuar(int tempo) {
+		robo.acionarMotores(VELPADRAO * (-1),VELPADRAO * (-1));
+		delay(tempo);
+	}
+
+	/*-------------------------------------*/
+
+	void alinhar(String sensorViuPreto) {
+		switch (sensor) {
+			case "dir": // 
+				virarEsquerda();
+				break;
+			case "esq":
+				virarDireita();
+				break;
+		}
+	}
+
+	/*------------------------------------*/
+
+	void Movimentacao::setVelPadrao(int vel) { VELPADRAO = vel; } 
+	/*         ^                  ^                  ^         
+		Essa função foi criada para a facilitação da determinação
+		da velocidade padrão.
+	*/
 };
 
 //static Movimentacao motores;
