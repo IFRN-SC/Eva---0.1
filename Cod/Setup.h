0d27f26d80a35d12837a6d0e01ac44daba992fcf
isaacmarlon <isaacmarlondasilvalourenco@gmail.com>
2018-04-12 02:40:02 -0300

    Reajustes para usar a calibracao - Setup.h

--- Cod/Setup.h
+++ Cod/Setup.h
@@ -3,18 +3,16 @@
 /**** Sobre:
 	 
 	Supre a necessidade de setar algum valor ou até mesmo
-	alguma função indo até a classe respectiva.
+	alguma função indo até a classe respectiva. Preguiça ^_^
 
 ****/
 
-//#include <robo_hardware.h>
+/*#include <robo_hardware.h>
+#include "Estrategia.h"
+#include "Sensores.h"
+#include "Sonares.h"*/
 
-//#include "Calibracao.h"
-
-
-//#include "Estrategia.h"
-//#include "Sensores.h"
-//#include "Sonares.h"
+#include "Calibracao.h"
 #include "Motores.h"
 #include "Obstaculo.h"
 #include "Leds.h"
@@ -26,27 +24,22 @@ public:
 	void pinarLeds(bool value) { // Define a pinagem para os LEDs.
 		if (value) 
 		leds.pinar(); 
-	} 
+	}
 	void calibrar(bool value) { // Entra no modo de Calibração.
-		/*if (value)
-		calibrar.iniciar();*/ 
-	} 
-	
+		if (value)
+		calibracao.iniciar();
+	}
 	void motoresVelPadrao(int VEL_SET) { // Seta o VELPADRAO em "Motores.h".
 		motores.setVelPadrao(VEL_SET); 
 	} 	
 	void distanciaObstaculo(int DISTANCIA_SET) { // Seta DISTANCIA_OBSTACULO em ...
 		obstaculo.setDistancia(DISTANCIA_SET);   // ... "Obstaculo.h".
 	}
-	void separacaoBrancoPreto(int valor){
-		sensor.setSeparacao(valor);
-	}
 
 private:
 
-	//Sonares sonar;
 	Leds leds;
-	//Calibrar calibrar;
+	Calibracao calibracao;
 	Motores motores;
 	Obstaculo obstaculo;
 };
