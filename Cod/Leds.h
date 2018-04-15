6826bb92b44d2001a67ee0b95445af081e54dc5c
isaacmarlon <isaacmarlondasilvalourenco@gmail.com>
2018-04-14 22:59:16 -0300

    Classe sendo preparada para comunicação - Leds.h

--- Cod/Leds.h
+++ Cod/Leds.h
@@ -4,9 +4,42 @@
 class Leds {
 public:
 	void pinar(){
+		// Futuro uso para comunicação por leds
+
+		/*pinMode(LED_maisEsq, OUTPUT);		
+		pinMode(LED_Esq, OUTPUT);
+		pinMode(LED_Dir, OUTPUT);
+		pinMode(LED_maisDir, OUTPUT);*/ 
+		
 		pinMode(10, OUTPUT);
 		pinMode(11, OUTPUT);
 	}
+
+	/*
+	void piscar(int led){
+		switch (led) {
+			case 1: // maisEsq viu preto.
+				digitalWrite(LED_maisEsq, HIGH);
+				delay(500);
+				digitalWrite(LED_maisEsq, LOW);
+				break;
+			case 2:
+				digitalWrite(LED_Esq, HIGH);
+				delay(500);
+				digitalWrite(LED_Esq, LOW);
+				break;
+			case 3:
+				digitalWrite(LED_Dir, HIGH);
+				delay(500);
+				digitalWrite(LED_Dir, LOW);
+				break;
+			case 4:
+				digitalWrite(LED_maisDir, HIGH);
+				delay(500);
+				digitalWrite(LED_maisDir, LOW);
+				break;
+		}
+	}*/
 };
 
 #endif
