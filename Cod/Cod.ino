733f2719548451c82aca4c2e838f9b6b935991ca
isaacmarlon <isaacmarlondasilvalourenco@gmail.com>
2018-04-15 20:04:38 -0300

    Retirado vestígios de "Setup.h", retirado função desnecessárias.

--- Cod/Cod.ino
+++ Cod/Cod.ino
@@ -1,16 +1,11 @@
-#include "Setup.h"
 #include "Estrategia.h"
 
-
 Estrategia estrategia;
-Motores motores;
 
 void setup(){
 	Serial.begin(9600);
 	robo.configurar(false);
 	estrategia.calibrar(false);
-	estrategia.setVelocidade(40);
-	estrategia.distanciaObstaculo(10); 
 }
 
 void loop(){
