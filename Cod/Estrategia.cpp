93d280b6b05c7ee492ba4a134ffc79adc647aa98
isaacmarlon <isaacmarlondasilvalourenco@gmail.com>
2018-04-15 23:45:58 -0300

    Atualização Estrategia.cpp - (Impulso) Novas funções e nova estrategia para seguir linha

--- Cod/Estrategia.cpp
+++ Cod/Estrategia.cpp
@@ -1,12 +1,14 @@
 #include "Estrategia.h"
 
 void Estrategia::run() {
-	if (sonar.viuObstaculo(DISTANCIA_OBSTACULO)) {
-		obstaculo.iniciar(DISTANCIA_OBSTACULO);
-		/*
-			A partir de qual DISTANCIA_OBSTACULO devo
-		  	começar a desviar?
-		*/
+ 	/* !-- Se o sonar frontal retornar um valor menor ou igual
+ 		   ao valor de DISTANCIA_OBSTACULO, o robô começará
+ 		   a desviar do obstáculo.
+
+ 		   Se não, somente seguirá a linha.
+ 	*/
+	if (sensores.sonarViuObstaculo(DISTANCIA_OBSTACULO)) {
+		disviarObstaculo();
 	} 
 	else {
 		seguirLinha();
@@ -14,18 +16,64 @@ void Estrategia::run() {
 }
 
 void Estrategia::seguirLinha() {
-	if(sensor.dirViuPreto()) {
-		motores.virarEsquerda();
-	} 
-	else if(sensor.esqViuPreto()) {
-		motores.virarDireita();
-	}
-	else if(sensores.todosBrancos() ||
-			sensores.todosPretos()
-			)
+	/* Se todos virem branco ou preto, motores avançam.
+	*/	
+	if (sensores.branco_branco_branco_branco()
+	    || 
+	    sensores.preto_preto_preto_preto()) 
 	{
-		motores.emFrente();
-	} else {
+		motores.avancar();
+	}
+	else if (sensores.branco_preto_branco_branco()) {
+		while (sensores.esqPreto()) {
+			motores.virarDireita();	
+		}
+		motores.parar();
+		delay(500);
+		//passarVerde('L');
+		impulso();
+	}	
+	else if (sensor.branco_branco_preto_branco()) {
+		while(sensores.dirPreto()) {
+			motores.virarEsquerda();	
+		}
+		motores.parar();
+		delay(500);
+		//passarVerde('R');
+		impulso();
+	}
+	else if (sensor.preto_preto_branco_branco()){
+		while(!sensores.branco_branco_branco_branco()){
+			motores.virarDireita();
+		}
+		motores.parar();
+		while(!sensores.preto_preto_branco_branco()){
+			motores.virarDireita();
+		}
+		motores.parar();
+		while(!sensore.branco_branco_branco_branco()) {
+			motores.avancar();
+		}
+		motores.parar();
+		impulso();
+	}
+	else if (sensor.branco_branco_preto_preto()){
+		while(!sensores.branco_branco_branco_branco()){
+			motores.virarEsquerda();
+		}
+		motores.parar();
+		while(!sensores.branco_branco_preto_preto()){
+			motores.virarEsquerda();
+		}
+		motores.parar();
+		while(!sensore.branco_branco_branco_branco()) {
+			motores.avancar();
+		}
+		motores.parar();
+		impulso();
+	}	
+	/*
+	else {
 		motores.parar(500);
 		while(1){
 			motores.virarEsquerda();
@@ -34,29 +82,41 @@ void Estrategia::seguirLinha() {
 			delay(500);
 		}
 	}
-	/*else {
-		curvas();	
-	}*/
-}
-/*
-void Estrategia::curvas() {
-	if(sensores.preto_preto_branco_branco()) {
-		motores.virarEsquerda();
-	}
-	else if (sensores.branco_branco_preto_preto()){
-		motores.virarDireita();
-	}
+	
 	else {
-		passarVerde();
+		passarVerde();	
+	}
+	*/
+}
+
+/*
+void Estrategia::passarVerde(int lado) {
+	switch (lado){
+		case 'L': // Esquerda
+			if (!sensores.branco_branco_branco_branco() &&
+				!sensores.preto_preto_preto_preto()) 
+			{
+				while(!sensores.preto_preto_preto_preto()){
+					motores.virarEixoEsq();
+				}
+			}
+			break;
+		case 'R':
+
 	}
 }
 */
-/*
-void Estrategia::passarVerde() {
-	if (sensor.esqViuPreto()){
-		while (!sensores.todosPretos()){
-			motores.emFrente();
-		}
 
+void Estrategia::impulso() {
+	while (sensores.branco_branco_branco_branco()
+		  || 
+		  sensores.preto_preto_preto_preto()
+		  && 
+		  (!sensores.sonarViuObstaculo(DISTANCIA_OBSTACULO))) 
+	{
+		robo.acionarMotores(60,60);	
 	}
-}*/
+	motores.parar();
+	delay(500);
+	run();
+}
