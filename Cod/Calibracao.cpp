28f5d63e4749a34cb573eb6b887e9eb1025bc2b5
robotica@sc111443 <robotica@sc111443>
2018-04-17 10:27:51 -0300

    Atualização Calibracao.h

--- Cod/Calibracao.cpp
+++ Cod/Calibracao.cpp
@@ -2,6 +2,10 @@
 
 Calibracao::Calibracao() {
 	estiverCalibrando = true;
+	minimoEsq2 = 9999;
+	minimoEsq = 9999;
+	minimoDir = 9999;
+	minimoDir2 = 9999;
 }
 
 void Calibracao::run(){
@@ -35,23 +39,43 @@ void Calibracao::menuCalibracao(){
 }
 
 void Calibracao::minimoBranco(){
-	int resp = 0;
-	while (resp != 0){
+	char escolha = ' ';
+	while (1){
 		Serial.println("CALIBRAR MINIMO BRANCO: ");
 		Serial.println();
 		Serial.println("POSICIONES OS SENSORES NO BRANCO!");
 		Serial.println();
-		Serial.println("INSIRA QUALQUER COISA PARA PEGAR VALORES");
-		Serial.println("[0] SAIR");
+		Serial.println("INSIRA QUALQUER COISA PARA PEGAR OS VALORES");
+		Serial.println("[S] VOLTAR");
 		Serial.println();
 		Serial.print("R = ");
 		esperarParaLer();
 		resp = Serial.read();
+		
+		if (escolha == 'S') menuCalibracao();
+		else calculeMinimo(robo.lerSensorLinhaEsq2(),robo.lerSensorLinhaEsq(),robo.lerSensorLinhaDir(),robo.lerSensorLinhaDir2);
 	}
 }
+void Calibracao::maximoPreto(){}
 
+void Calibracao::calculeMinimo(int valorEsq2,int valorEsq,int valorDir,int valorDir2){
+	if (valorEsq2 < minimoEsq2) {
+		minimoEsq2 = valorEsq2;
+	}
+	if (valorEsq < minimoEsq) {
+		minimoEsq = valorEsq;
+	}
+	if (valorDir < minimoDir) {
+		minimoDir = valorDir;
+	}
+	if (valorDir2 < minimoDir2) {
+		minimoDir2 = valorDir2;
+	}
+}
 
 
 void Calibracao::esperarParaLer(){
 	while(!Serial.available()){}
 }
+
+
