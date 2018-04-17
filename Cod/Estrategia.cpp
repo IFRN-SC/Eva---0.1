d102cc33b57b87cbd61b2e8247f74c7e024301d0
robotica@sc111443 <robotica@sc111443>
2018-04-16 21:19:54 -0300

    Consequências da incorporação das funções para Obstáculo - Estrategia.cpp

--- Cod/Estrategia.cpp
+++ Cod/Estrategia.cpp
@@ -8,13 +8,13 @@ void Estrategia::run() {
  		   Se não, somente seguirá a linha.
  	*/
 	if (sensores.sonarViuObstaculo(DISTANCIA_OBSTACULO))
-		disviarObstaculo();
+		desviarObstaculo();
 	else
 		seguirLinha();
 }
 
 void Estrategia::calibrar(bool value) {
-	if (value)
+	if (value) 
 		calibracao.run();
 }
 
@@ -28,51 +28,49 @@ void Estrategia::seguirLinha() {
 		motores.avancar();
 	}
 	else if (sensores.branco_preto_branco_branco()) {
-		while (sensores.esqPreto()) {
+		while (sensores.esqViuPreto()) {
 			motores.virarDireita();	
 		}
-		motores.parar();
-		delay(500);
+		motores.parar(500);
 		//passarVerde('L');
 		impulso();
 	}	
-	else if (sensor.branco_branco_preto_branco()) {
-		while(sensores.dirPreto()) {
+	else if (sensores.branco_branco_preto_branco()) {
+		while(sensores.dirViuPreto()) {
 			motores.virarEsquerda();	
 		}
-		motores.parar();
-		delay(500);
+		motores.parar(500);
 		//passarVerde('R');
 		impulso();
 	}
-	else if (sensor.preto_preto_branco_branco()){
+	else if (sensores.preto_preto_branco_branco()){
 		while(!sensores.branco_branco_branco_branco()){
 			motores.virarDireita();
 		}
-		motores.parar();
+		motores.parar(0);
 		while(!sensores.preto_preto_branco_branco()){
 			motores.virarDireita();
 		}
-		motores.parar();
-		while(!sensore.branco_branco_branco_branco()) {
+		motores.parar(0);
+		while(!sensores.branco_branco_branco_branco()) {
 			motores.avancar();
 		}
-		motores.parar();
+		motores.parar(0);
 		impulso();
 	}
-	else if (sensor.branco_branco_preto_preto()){
+	else if (sensores.branco_branco_preto_preto()){
 		while(!sensores.branco_branco_branco_branco()){
 			motores.virarEsquerda();
 		}
-		motores.parar();
+		motores.parar(0);
 		while(!sensores.branco_branco_preto_preto()){
 			motores.virarEsquerda();
 		}
-		motores.parar();
-		while(!sensore.branco_branco_branco_branco()) {
+		motores.parar(0);
+		while(!sensores.branco_branco_branco_branco()) {
 			motores.avancar();
 		}
-		motores.parar();
+		motores.parar(0);
 		impulso();
 	}	
 	/*
@@ -119,7 +117,82 @@ void Estrategia::impulso() {
 	{
 		robo.acionarMotores(60,60);	
 	}
-	motores.parar();
-	delay(500);
-	run();
+	motores.parar(500);
+}
+
+void Estrategia::desviarObstaculo(){
+	
+	//piscarLeds();
+	
+	bool pelaDireita; // Se o robô está indo realmente pela direita.
+
+	motores.parar(150);
+	motores.recuar(180);
+	
+	while (!sensores.maisDirViuPreto()){
+		robo.acionarMotores(0,VEL * (-1));
+	}
+
+	/**** Verificar melhor lado para ir ****/
+
+	if (sensores.sonarViuObstaculo(3 * DISTANCIA_OBSTACULO)){  // Se é melhor ir para a direita
+		pelaDireita = true;
+		alinharObstaculo('R'); // Right - Direita
+	} 
+	else {	// Se é melhor ir para a esquerda
+
+		while (!sensores.maisDirViuPreto()){
+			robo.acionarMotores(0,VEL);
+		}
+
+		alinharObstaculo('L'); // Left - Esquerda
+	}
+
+	motores.parar(1000);
+	
+	motores.avancar();
+
+
+	// ENROLADA EXPONENCIAL
+	
+	if(pelaDireita){
+		for (int i=10; i!=0; --i){
+			robo.acionarMotores((VEL * (i/100)),VEL);
+			delay(200);	
+		}
+	} 
+	else {
+		for (int i=10; i!=0; --i){
+			robo.acionarMotores(VEL,(VEL * (i/100)));
+			delay(200);	
+		}
+	}
+	
+	motores.avancar();
+}
+
+void Estrategia::alinharObstaculo(char lado){
+	switch (lado){
+		case 'R':
+			while (obstaculoDesalinhado()) {
+				robo.acionarMotores((VEL * 0.10) * (-1),VEL * (-1)); 
+				/*  ^				^				^	 		^  
+					Motor contrário recua com 10% de VEL.
+				*/
+			}
+			break;
+		case 'L':
+			while (obstaculoDesalinhado()) {
+				robo.acionarMotores(VEL * (-1),(VEL * 0.10) * (-1)); 
+				/*  ^				^				^	 		^  
+					Motor contrário recua com 10% de VEL.
+				*/
+			}
+			break;
+	}
+	
+}
+
+bool Estrategia::obstaculoDesalinhado(){
+	return (sensores.branco_branco_branco_branco());
 }
